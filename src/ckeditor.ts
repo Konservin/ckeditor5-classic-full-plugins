ced517bcd32ab405dbdc85be65a728f2330c4ade
Ervin Bernhardt <ervin.bernhardt@gmail.com>
2024-09-08 18:18:59 +0300

    Remove Title, add Bold, fix dependencies

--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -18,138 +18,22 @@ import {
 	Superscript,
 	Underline
 } from '@ckeditor/ckeditor5-basic-styles';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
-import type { EditorConfig } from '@ckeditor/ckeditor5-core';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
-import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Highlight } from '@ckeditor/ckeditor5-highlight';
-import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
-import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
-import {
-	DataFilter,
-	DataSchema,
-	GeneralHtmlSupport,
-	HtmlComment
-} from '@ckeditor/ckeditor5-html-support';
-import {
-	Image,
-	ImageCaption,
-	ImageInsert,
-	ImageResize,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload
-} from '@ckeditor/ckeditor5-image';
-import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
-import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
-import { Mention } from '@ckeditor/ckeditor5-mention';
-import { PageBreak } from '@ckeditor/ckeditor5-page-break';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
-import { StandardEditingMode } from '@ckeditor/ckeditor5-restricted-editing';
-import { SelectAll } from '@ckeditor/ckeditor5-select-all';
-import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
-import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
-import {
-	SpecialCharacters,
-	SpecialCharactersArrows,
-	SpecialCharactersCurrency,
-	SpecialCharactersEssentials,
-	SpecialCharactersLatin,
-	SpecialCharactersMathematical,
-	SpecialCharactersText
-} from '@ckeditor/ckeditor5-special-characters';
-import { Style } from '@ckeditor/ckeditor5-style';
-import {
-	Table,
-	TableCaption,
-	TableCellProperties,
-	TableColumnResize,
-	TableProperties,
-	TableToolbar
-} from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
-import { EditorWatchdog } from '@ckeditor/ckeditor5-watchdog';
-import { WordCount } from '@ckeditor/ckeditor5-word-count';
+
 
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
 class CKEditor extends ClassicEditor {
 	static override builtinPlugins = [
 		Alignment,
-		AutoLink,
 		Autoformat,
 		Autosave,
 		Bold,
 		Code,
-		CodeBlock,
-		DataFilter,
-		DataSchema,
-		Essentials,
-		FindAndReplace,
-		FontBackgroundColor,
-		FontColor,
-		FontFamily,
-		FontSize,
-		GeneralHtmlSupport,
-		Heading,
-		Highlight,
-		HorizontalLine,
-		HtmlComment,
-		HtmlEmbed,
-		Image,
-		ImageCaption,
-		ImageInsert,
-		ImageResize,
-		ImageStyle,
-		ImageToolbar,
-		ImageUpload,
-		Indent,
-		IndentBlock,
 		Italic,
-		Link,
-		LinkImage,
-		List,
-		ListProperties,
-		MediaEmbed,
-		MediaEmbedToolbar,
-		Mention,
-		PageBreak,
-		Paragraph,
-		PasteFromOffice,
-		RemoveFormat,
-		SelectAll,
-		ShowBlocks,
-		SimpleUploadAdapter,
-		SourceEditing,
-		SpecialCharacters,
-		SpecialCharactersArrows,
-		SpecialCharactersCurrency,
-		SpecialCharactersEssentials,
-		SpecialCharactersLatin,
-		SpecialCharactersMathematical,
-		SpecialCharactersText,
-		StandardEditingMode,
 		Strikethrough,
-		Style,
 		Subscript,
 		Superscript,
-		Table,
-		TableCaption,
-		TableCellProperties,
-		TableColumnResize,
-		TableProperties,
-		TableToolbar,
-		TextTransformation,
-		TodoList,
 		Underline,
-		WordCount
 	];
 
 	static override defaultConfig = {
@@ -221,4 +105,4 @@ class CKEditor extends ClassicEditor {
 	};
 }
 
-export default { CKEditor, EditorWatchdog };
+export default { CKEditor };
