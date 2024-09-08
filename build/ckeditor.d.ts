48df2eca4344740e32010b35e53bf56b6b9cec06
Ervin Bernhardt <ervin.bernhardt@gmail.com>
2024-09-08 19:14:32 +0300

    Remove Title, add Bold, fix dependencies

--- build/ckeditor.d.ts
+++ build/ckeditor.d.ts
@@ -3,12 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Autosave } from '@ckeditor/ckeditor5-autosave';
-import { Bold, Code, Italic, Strikethrough, Subscript, Superscript, Underline } from '@ckeditor/ckeditor5-basic-styles';
 declare class CKEditor extends ClassicEditor {
-    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof Autosave | typeof Bold | typeof Code | typeof Italic | typeof Strikethrough | typeof Subscript | typeof Superscript | typeof Underline)[];
     static defaultConfig: {
         toolbar: {
             items: string[];
--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -6,35 +6,10 @@
 //import { Editor } from '@ckeditor/ckeditor5-core';
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Autosave } from '@ckeditor/ckeditor5-autosave';
-import {
-	Bold,
-	Code,
-	Italic,
-	Strikethrough,
-	Subscript,
-	Superscript,
-	Underline
-} from '@ckeditor/ckeditor5-basic-styles';
-
 
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
 
 class CKEditor extends ClassicEditor {
-	static override builtinPlugins = [
-		Alignment,
-		Autoformat,
-		Autosave,
-		Bold,
-		Code,
-		Italic,
-		Strikethrough,
-		Subscript,
-		Superscript,
-		Underline,
-	];
 
 	static override defaultConfig = {
 		toolbar: {
